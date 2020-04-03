bf18a90d69bf40af645b1af1f478190eba5f9ec3
puujeest <puujeest@gmail.com>
2020-04-03 09:28:09 +0800

    Programm duuslaa. 81 dugeer hicheel

--- app.js
+++ app.js
@@ -60,6 +60,22 @@ var uiController = (function() {
         unuudur.getFullYear() + " оны " + unuudur.getMonth() + " сарын";
     },
 
+    changeType: function() {
+      var fields = document.querySelectorAll(
+        DOMstrings.inputType +
+          "," +
+          DOMstrings.inputDescription +
+          "," +
+          DOMstrings.inputValue
+      );
+
+      nodListForeach(fields, function(el) {
+        el.classList.toggle("red-focus");
+      });
+
+      document.querySelector(DOMstrings.addBtn).classList.toggle("red");
+    },
+
     getInput: function() {
       return {
         type: document.querySelector(DOMstrings.inputType).value,
@@ -338,6 +354,10 @@ var appController = (function(uiController, financeController) {
         ctrlAddItem();
       }
     });
+
+    document
+      .querySelector(DOM.inputType)
+      .addEventListener("change", uiController.changeType);
 
     document
       .querySelector(DOM.containerDiv)
